d4a69ff87c76bd3d4da701cfccb44a6b3e32c8bc
chenmeng <2691357857@qq.com>
2023-05-16 23:33:41 +0800

    第六阶段

--- src/main/java/com/chenmeng/project/CmApplication.java
+++ src/main/java/com/chenmeng/project/CmApplication.java
@@ -1,11 +1,13 @@
 package com.chenmeng.project;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.chenmeng.project.mapper")
+@EnableDubbo
 public class CmApplication {
 
     public static void main(String[] args) {
