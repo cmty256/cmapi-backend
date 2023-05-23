10a40ccedbadda9c31724f4e6d1a8290fc9d76d7
chenmeng <2691357857@qq.com>
2023-05-23 10:39:43 +0800

    修改测试调用接口的 响应错误bug

--- cmapi-interface/src/main/java/com/chenmeng/controller/NameController.java
+++ cmapi-interface/src/main/java/com/chenmeng/controller/NameController.java
@@ -33,36 +33,6 @@ public class NameController {
 
     @PostMapping("/user")
     public String getNameByPostWithJson(@RequestBody User user, HttpServletRequest request) {
-        String accessKey = request.getHeader("accessKey");
-        // 防止中文乱码 -- 解码
-        String body = URLUtil.decode(request.getHeader("body"));
-        // 签名
-        String sign = request.getHeader("sign");
-        String nonce = request.getHeader("nonce");
-        String timestamp = request.getHeader("timestamp");
-
-        boolean hasBlank = StrUtil.hasBlank(accessKey, body, sign, nonce, timestamp);
-        // 判断是否 为空
-        if (hasBlank) {
-            return "无权限";
-        }
-        // TODO 使用 accessKey 去数据库查询 secretKey
-        // 假设查到的 secretKey 是 abc 进行加密得到签名 sign
-        String secretKey = "abc";
-        String sign1 = SignUtil.getSign(body, secretKey);
-        // 判断签名是否相等
-        if (!StrUtil.equals(sign, sign1)) {
-            return "无权限";
-        }
-        // TODO 判断随机数 nonce
-        // 时间戳是否为数字
-        if (!NumberUtil.isNumber(timestamp)) {
-            return "无权限";
-        }
-        // 5 分钟内的请求才有效
-        if (System.currentTimeMillis() - Long.parseLong(timestamp) > 5 * 60 * 1000) {
-            return "无权限";
-        }
         return "发送POST请求 JSON中你的名字是：" + user.getName();
     }
 
